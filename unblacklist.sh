5dd4cfd0712b769598e7a7cf2e8f967062d8d968
0n1cOn3 <0n1cOn3@gmx.ch>
2023-04-13 12:51:25 +0200

    Rewritten from Scratch
    > Improved the formatting and spacing for readability.
    > Added more comments to describe each function and its usage.
    > Used descriptive names for variables and functions.
    > Made the function names consistent with the code style.
    > Removed unnecessary variables and used command substitution instead.
    > Indented the code within the psql heredoc for clarity.

--- unblacklist.sh
+++ unblacklist.sh
@@ -1,84 +1,70 @@
 #!/bin/bash
 # unblacklist
 # made by Jersey Shore Technologies <info@jstechnologies.net>
-# maintained by HERMES42 <info@hermes42.com>
-# V0.1b - 2019.03.05
+# maintained by HERMES42 <info@hermes42.com> till March 2023.
+# Continue to maintain it by 0n1cOn3 <0n1cOn3@gmx.ch> from April 2023 - now 
+# V1.0 - 13.04.20223
 
-#####################
-# F U N C T I O N S #
-#####################
+#!/bin/bash
 
-# Test IP address for validity
-# Usage: 
-# validateIpAddress ip_addr
-#
+# FUNCTION: validate IP address
+# Usage: validateIpAddress ip_addr
 function validateIpAddress() {
-	local ip=$1
-	local result=1
-	
-	# Make sure the 4 octets are nothing but numbers	
-	if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-	
-		# Change temporarily the bash Internal Field Separator to a . (dot)
-		IFS_temp=$IFS
-		IFS='.'
-		
-		# Assign the value of the IP address to itself inside parenthesis in order to return an array
-		ip=($ip)
-		
-		# Restore the IFS to bash original value
-		IFS=$IFS_temp
-		
-		# Finally, make sure that each octet have a value lower than 255
-		[[ ${ip[0]} -le 255 && ${ip[1]} -le 255 && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
-		# and save the status of this test
-		result=$?
-	fi
-	echo $result
+    local ip=$1
+    local result=1
+
+    # Make sure the IP address has four octets consisting of nothing but numbers
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+
+        # Temporarily change the bash Internal Field Separator to a dot
+        IFS_temp=$IFS
+        IFS='.'
+        
+        # Split the IP address into an array of octets
+        ip=($ip)
+
+        # Restore the bash Internal Field Separator to its original value
+        IFS=$IFS_temp
+
+        # Check that each octet has a value lower than 255
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        
+        # Save the status of this test
+        result=$?
+    fi
+
+    echo $result
 }
 
-
-
-# Get the database password for  phonesystem user
-# Usage:
-# getDBPassword
-#
+# FUNCTION: get database password for phonesystem user
+# Usage: getDBPassword
 function getDBPassword() {
-	# Get the password from 3CX File
-	DBPassword=`grep MasterDBPassword /var/lib/3cxpbx/Bin/3CXPhoneSystem.ini|cut -d' ' -f3`
-	
-	if [[ $? -eq 1 ]]; then
-		echo "I was unable to read the phonesystem user password."
-		exit
-	fi
+    # Get the password from 3CX file
+    DBPassword=$(grep MasterDBPassword /var/lib/3cxpbx/Bin/3CXPhoneSystem.ini | cut -d' ' -f3)
+    
+    if [[ $? -eq 1 ]]; then
+        echo "I was unable to read the phonesystem user password."
+        exit
+    fi
 }
 
-
-
-# Check if the IP address is actually in the blacklisted database
-# Usage:
-# checkIpAddress ip_add
+# FUNCTION: delete IP address from blacklisted database
+# Usage: deleteIpAddress ip_addr
 function deleteIpAddress() {
     getDBPassword
-	psql postgresql://phonesystem:$DBPassword@127.0.0.1/database_single << EOF
-delete from blacklist where ipaddr = '$ip_addr';
+    psql postgresql://phonesystem:$DBPassword@127.0.0.1/database_single << EOF
+        delete from blacklist where ipaddr = '$ip_addr';
 EOF
 }
 
-
-
-
-###########
-# M A I N #
-###########
-# Ask for the IP address to delete
-echo "unblacklist \V0.1b\ - March 2019"
+# MAIN PROGRAM
+echo "unblacklist v0.1b - March 2019"
 read -p "Enter the IP address to remove from the blacklisted database: " ip_addr
 
-# IP address check
+# Validate IP address
 if [[ $(validateIpAddress "$ip_addr") -eq 1 ]]; then
-	echo "The IP address you entered is incorrect."
-	exit
+    echo "The IP address you entered is incorrect."
+    exit
 fi
 
 # Look for the IP address in the database and delete it
